a89512618684c9673445fe1c94df5238368d21b9
focustense <focustense@protonmail.com>
2025-01-23 16:25:07 -0500

    Cleanup: Remove now-unused `Cursor` and fix a few minor style issues.

--- RadialMenu/GameConstants.cs
+++ RadialMenu/GameConstants.cs
@@ -1,7 +1,7 @@
 ﻿namespace RadialMenu;
 
 // Things that are generally hardcoded in game logic but that we'd prefer not to hardcode here.
-internal class GameConstants
+internal static class GameConstants
 {
     public const int BACKPACK_PAGE_SIZE = 12;
 }
